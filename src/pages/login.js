931645a5a295848d762d61fc1f36ac0a131c96d9
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-08-18 19:40:36 +0300

    Signup and Auth State

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,105 +1,122 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import PropTypes from "prop-types"
-import AppIcon from "../images/icon.png"
-import axios from "axios"
-import {Link} from "react-router-dom"
-import CircularProgress from '@material-ui/core/CircularProgress';
+import PropTypes from "prop-types";
+import AppIcon from "../images/icon.png";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 // MUI stuff
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
-import TextField from "@material-ui/core/TextField"
-import Button from "@material-ui/core/Button"
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-const styles = {
-  form: {
-    textAlign: "center",
-  },
-  image: {
-    margin: '20px auto 20px auto'
-  },
-  pageTitle: {
-    margin: '10px auto 10px auto'
-  },
-  textField: {
-    margin: '10px auto 10px auto'
-  },
-  button: {
-    marginTop: 20,
-    position: 'relative',
-  },
-  customError: {
-    color: 'red',
-    fontSize: '0.8rem',
-    marginTop: 10
-  },
-  progress: {
-    position: 'absolute',
-  }
-}
+const styles = (theme) => ({
+  ...theme.login,
+});
 
 class login extends Component {
   constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       loading: false,
-      errors: {}
-    }
+      errors: {},
+    };
   }
 
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
-      loading: true
+      loading: true,
     });
     const userData = {
-      email: this.state.email, 
-      password: this.state.password
-    }
-    axios.post('/login', userData)
-      .then(res => {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/signup", userData)
+      .then((res) => {
         console.log(res.data);
+        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
         this.setState({
-          loading: false
+          loading: false,
         });
-        this.props.history.push('/');
+        this.props.history.push("/");
       })
-      .catch(err => {
-        console.log(err)
+      .catch((err) => {
+        console.log(err);
         this.setState({
           errors: err.response.data,
-          loading: false
-        })
-      })
-  }
+          loading: false,
+        });
+      });
+  };
 
   handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   render() {
-    const {classes} = this.props;
-    const {errors, loading} = this.state;
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
         <Grid item sm>
-          <img src={AppIcon} alt="monkey" className={classes.image}/>
-          <Typography variant="h2" className={classes.pageTitle}>Login</Typography>
+          <img src={AppIcon} alt="monkey" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
-            <TextField id="email" name="email" type="email" label="Email" className={classes.textField} helperText={errors.email} error={errors.email ? true : false} value={this.state.email} onChange={this.handleChange} fullWidth/>
-            <TextField id="password" name="password" type="password" label="Password" className={classes.textField} helperText={errors.password} error={errors.password ? true : false} value={this.state.password} onChange={this.handleChange} fullWidth/>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
             {errors.general && (
-              <Typography variant="body2" className={classes.customError}>{errors.general}</Typography>
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
             )}
-            <Button type="submit" variant="contained" color="primary" className={classes.button}>Login {loading && <CircularProgress size={30} className={classes.progress}/>}</Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Login{" "}
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
             <br />
-            <small>Don't have an account ? sign up <Link to="/signup">here</Link></small>
+            <small>
+              Don't have an account ? sign up <Link to="/signup">here</Link>
+            </small>
           </form>
         </Grid>
         <Grid item sm />
@@ -109,7 +126,7 @@ class login extends Component {
 }
 
 login.propTypes = {
-  classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(login);
