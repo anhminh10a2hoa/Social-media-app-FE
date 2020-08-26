67c2b80e03b77b766cc6732f8d4f3250eea1e9dc
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-08-27 02:23:10 +0300

    Like and Unlike function

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,21 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 import Scream from "../components/Scream";
 import Profile from "../components/Profile";
+
+import {connect} from 'react-redux';
+import {getScreams} from '../redux/actions/dataActions';
 class home extends Component {
-  state = {
-    screams: null,
-  };
   componentDidMount() {
-    axios
-      .get("/screams")
-      .then((res) => {
-        this.setState({ screams: res.data });
-      })
-      .catch((err) => console.log(err));
+    this.props.getScreams();
   }
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => (
+    const {screams, loading} = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map((scream) => (
         <Scream key={scream.screamId} scream={scream} />
       ))
     ) : (
@@ -37,4 +34,13 @@ class home extends Component {
   }
 }
 
-export default home;
+home.propTypes = {
+  data: PropTypes.object.isRequired,
+  getScreams: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data,
+})
+
+export default connect(mapStateToProps, {getScreams})(home);
---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,36 @@
+import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM } from '../types'
+
+const initialState = {
+  screams: [],
+  scream: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_SCREAMS:
+      return {
+        ...state,
+        screams: action.payload,
+        loading: false,
+      }
+    case LOADING_DATA:
+      return {
+        ...state,
+        loading: true
+      }
+    case LIKE_SCREAM:
+    case UNLIKE_SCREAM:
+      let index = state.screams.findIndex(
+        (scream) => scream.screamId === action.payload.screamId
+      );
+      state.screams[index] = action.payload;
+      if (state.scream.screamId === action.payload.screamId) {
+        state.scream = action.payload;
+      }
+      return {
+        ...state
+      };
+    default: return state
+  }
+}
